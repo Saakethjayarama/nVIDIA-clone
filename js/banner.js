60ac19c35953378a2b5b19eb3dd358eba2fef846
saaketh <saakethaj@gmail.com>
2021-04-15 20:11:52 +0530

    corousel mobile done

---
+++ js/banner.js
@@ -0,0 +1,36 @@
+current = 0;
+
+const banner = document.getElementById("banner-container");
+const contents = document.querySelectorAll("#banner-content");
+const circles = document.querySelectorAll(".circle");
+const imagesList = [
+  "banner_one.jpg",
+  "banner_two.png",
+  "banner_three.jpg",
+  "banner_four.jpg",
+  "banner_five.jpg",
+];
+
+setInterval(() => {
+  // get current content and circle
+  let thisContent = contents[current];
+  let thisCircle = circles[current];
+
+  if (current == 4) {
+    current = 0;
+  } else {
+    current++;
+  }
+
+  // change background
+  const host = window.location.href;
+  banner.style.backgroundImage = host + "images/" + imagesList[current];
+
+  // add hidden class to current content element
+  thisContent.classList.add("hidden");
+  contents[current].classList.remove("hidden");
+
+  // change green circle
+  thisCircle.classList.remove("green");
+  circles[current].classList.add("green");
+}, 5000);
