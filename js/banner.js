d2e224e86983fb0e88051e163e23d115656e28ee
saaketh <saakethaj@gmail.com>
2021-04-15 20:33:40 +0530

    banner img transition added

--- js/banner.js
+++ js/banner.js
@@ -1,36 +1,39 @@
-current = 0;
+window.onload = function () {
+  let current = 0;
 
-const banner = document.getElementById("banner-container");
-const contents = document.querySelectorAll("#banner-content");
-const circles = document.querySelectorAll(".circle");
-const imagesList = [
-  "banner_one.jpg",
-  "banner_two.png",
-  "banner_three.jpg",
-  "banner_four.jpg",
-  "banner_five.jpg",
-];
+  const banner = document.getElementById("banner-container");
+  const contents = document.querySelectorAll("#banner-content");
+  const circles = document.querySelectorAll(".circle");
+  const imagesList = [
+    "banner_one.jpg",
+    "banner_two.png",
+    "banner_three.jpg",
+    "banner_four.jpg",
+    "banner_five.jpg",
+  ];
 
-setInterval(() => {
-  // get current content and circle
-  let thisContent = contents[current];
-  let thisCircle = circles[current];
+  setInterval(() => {
+    // get current content and circle
+    let thisContent = contents[current];
+    let thisCircle = circles[current];
 
-  if (current == 4) {
-    current = 0;
-  } else {
-    current++;
-  }
+    if (current == 4) {
+      current = 0;
+    } else {
+      current++;
+    }
 
-  // change background
-  const host = window.location.href;
-  banner.style.backgroundImage = host + "images/" + imagesList[current];
+    banner.animate([{ opacity: 0 }, { opacity: 1 }], 1000);
+    // change background
+    const host = window.location.href;
+    banner.style.backgroundImage = `url(${host}images/${imagesList[current]})`;
 
-  // add hidden class to current content element
-  thisContent.classList.add("hidden");
-  contents[current].classList.remove("hidden");
+    // add hidden class to current content element
+    thisContent.classList.add("hidden");
+    contents[current].classList.remove("hidden");
 
-  // change green circle
-  thisCircle.classList.remove("green");
-  circles[current].classList.add("green");
-}, 5000);
+    // change green circle
+    thisCircle.classList.remove("green");
+    circles[current].classList.add("green");
+  }, 5000);
+};
